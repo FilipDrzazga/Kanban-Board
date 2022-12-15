61a143a0b739bfc5b1d2c457f8be077023787637
Filip <43039909+Urfisior@users.noreply.github.com>
2022-12-15 21:06:55 +0000

    add react & react-dom dependencies

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,4 +1,4 @@
-import React, { useContext,useEffect } from 'react';
+import React, { useContext } from 'react';
 
 import Column from './Column';
 import ColumnContext from '../Context/ColumnContext';
